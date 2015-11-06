7238d3c3afbda663afd612dcdbc34f995ff44e06
Michael Mrozek <git@mrozekma.com>
2015-11-06 17:46:39 -0500

    Store events in a database after parsing them once

--- spades.py
+++ spades.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import re
+import shelve
 
 from GameConstructor import GameConstructor
 
@@ -57,32 +58,30 @@ def fixSuits(str):
 		str = str.replace(unicode, plain)
 	return str
 
-events = []
-with open('log') as f:
-	# for line in f:
-	while True:
-		off = f.tell()
-		line = f.readline()
-		if line == '':
-			break
-		line = fixSuits(line)
-		for pattern, fns in eventPatterns:
-			match = pattern.match(line)
-			if match:
-				for fn in fns:
-					g = match.groupdict() # Copy; the original match groupdict is not changed below
-					event = {'ts': g['ts'], 'off': off}
-					del g['ts']
-					event.update(fn(**g))
-					events.append(event)
+d = shelve.open('db')
+if 'events' not in d:
+	events = []
+	with open('/tmp/#rhspades.log') as f:
+		# for line in f:
+		while True:
+			off = f.tell()
+			line = f.readline()
+			if line == '':
 				break
+			line = fixSuits(line)
+			for pattern, fns in eventPatterns:
+				match = pattern.match(line)
+				if match:
+					for fn in fns:
+						g = match.groupdict() # Copy; the original match groupdict is not changed below
+						event = {'ts': datetime.strptime(g['ts'], '%Y-%m-%d %H:%M:%S'), 'off': off}
+						del g['ts']
+						event.update(fn(**g))
+						events.append(event)
+					break
+	d['events'] = events
 
-from pprint import pprint
-pprint(events)
-
-print
-print '-'*80
-print
+events = d['events']
 
 handler = GameConstructor()
 for event in events:
