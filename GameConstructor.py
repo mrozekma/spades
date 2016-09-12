mrozekma/spades

commit 6ead673daaf21c4038df147c02702dbff59d4f57

Show who has passed when exchanging cards during a nil

GameConstructor.py
@@ -123,6 +123,21 @@ class GameConstructor:
 			if event['type'] == 'bid' and event['bid'] in ('nil', 'blind'):
 				return
 
+			if event['type'] == 'passing':
+				if not hasattr(self, 'passed'):
+					self.passed = {}
+					self.thisPlayStart = event['ts'] # This is used by Game.runState
+				self.passed.update({who: False for who in event['who']})
+				return
+			if event['type'] == 'passed':
+				if event['finished']:
+					del self.passed
+					del self.thisPlayStart
+				elif event['who'] not in self.passed:
+					raise RuntimeError("Bad pass")
+				else:
+					self.passed[event['who']] = True
+				return
 			if event['type'] == 'playing':
 				self.currentPlayer = event['who']
 				if not hasattr(self, 'currentTrick'):

Data.py
@@ -118,6 +118,9 @@ class Game:
 			                        ("%s, and %s to %d" % (', '.join(rtn['pregame_players'][:-1]), rtn['pregame_players'][-1], self.goal)))
 		else:
 			rtn['description'] = ["Round %d" % len(self.rounds)]
+			if hasattr(self, 'gameCon') and hasattr(self.gameCon, 'passed'):
+				rtn['passed'] = self.gameCon.passed
+				rtn['turn_started'] = dtToJSTime(self.gameCon.thisPlayStart)
 			# Round data is in round player order, but the client needs it in game player order
 			def order(data):
 				data = {player: v for player, v in zip(self.currentRound.players, data)}

EventThread.py
@@ -48,21 +48,15 @@ eventPatterns = {
 	"(?P<team>TEAM) (?:make their bid|go bust): (?P<taken>NUMBER)/(?P<bid>NUMBER)": lambda team, taken, bid: {'type': 'round_summary', 'team': team, 'taken': int(taken), 'bid': int(bid)},
 	"(?P<user>USER) goes nil!": lambda user: {'type': 'nil_signal', 'who': user, 'bid': 'nil'},
 	"(?P<user>USER) goes blind nil!": lambda user: {'type': 'nil_signal', 'who': user, 'bid': 'blind'},
-
-	# Old/buggy message forms
-	# "(?P<user1>USER)/(?P<user2>USER) bid (?P<bid1>NUMBER|nil|blind)/(?P<bid2>NUMBER|nil|blind), (?P<user3>USER)/(?P<user4>USER) bid (?P<bid3>NUMBER|nil|blind)/(?P<bid4>NUMBER|nil|blind), (?P<bags>NUMBER) bags remain": lambda user1, user2, user3, user4, bid1, bid2, bid3, bid4, bags: {'type': 'bid_recap', 'bids': {user1: bid1 if bid1 in ('nil', 'blind') else int(bid1), user2: bid2 if bid2 in ('nil', 'blind') else int(bid2), user3: bid3 if bid3 in ('nil', 'blind') else int(bid3), user4: bid4 if bid4 in ('nil', 'blind') else int(bid4)}},
-	# "(.*) and (.*) are now known as .+": None, # .* because in one buggy case in 20160116_035923.log the player names were empty
-	# "(?P<user1>USER)/(?P<user2>USER) are now boring": lambda user1, user2: {'type': 'teamname', 'who': (user1, user2), 'name': None},
-	# "It's tie! Playing an extra round!": None,
+	"(?P<user1>USER)/(?P<user2>USER): select a card to pass \\(/msg USER \\.pass <card>\\)": lambda user1, user2: {'type': 'passing', 'who': (user1, user2)},
+	"(?P<user>USER) passes a card": lambda user: {'type': 'passed', 'who': user, 'finished': False},
+	"Cards have been passed!": lambda: {'type': 'passed', 'finished': True},
 
 	# These are unnecessary messages and generate no events
 	"Round over!": None,
 	"TEAM lead NUMBER to NUMBER of NUMBER": None,
 	"tied at NUMBER of NUMBER": None,
 	"TEAM bag (?:way )?out": None,
-	"USER/USER: select a card to pass \\(/msg USER \\.pass <card>\\)": None,
-	"USER passes a card": None,
-	"Cards have been passed!": None,
 	"USER (?:makes|fails at|makes blind|fails miserably at blind) nil!": None,
 	"USER (?:\\(USER\\) )?can (?:now|no longer) play for USER": None,
 	"USER allowed:.*": None,

handlers/game.py
@@ -51,6 +51,8 @@ def game(handler, name):
 	for seat in ('south', 'west', 'north', 'east'):
 		print "<div class=\"seat seat-%s seat-open\">" % seat
 		print   "<div class=\"tags\">"
+		print     "<span class=\"label label-danger tag-passing\">Passing</span>"
+		print     "<span class=\"label label-success tag-passed\">Passed</span>"
 		print     "<span class=\"label label-danger tag-turn\">Turn</span>"
 		print     "<span class=\"label label-success tag-winning\">Winning</span>"
 		print     "<span class=\"label label-primary tag-lead\">Lead</span>"

static/game.js
@@ -214,6 +214,8 @@ $(document).ready(function() {
 			set_player(seat, player);
 			set_player($($('table.past-tricks tr th')[i + 1]), player);
 
+			$('.tag-passing', seat).toggle('passed' in data && !data['passed'][player]);
+			$('.tag-passed', seat).toggle('passed' in data && data['passed'][player]);
 			$('.tag-lead', seat).toggle(player != null && data['leader'] == player);
 			$('.tag-turn', seat).toggle(player != null && data['turn'] == player);
 			$('.tag-winning', seat).toggle(player != null && data['winning'] == player);
@@ -245,14 +247,15 @@ $(document).ready(function() {
 		});
 
 		// It's possible (if unlikely) to have a turn field but no turn_started if we happen to pull from the server at the wrong instant
-		anchor = $($('.tag-turn:visible')[0]);
-		if(data['turn_started']) {
+		anchor = $('.tag-turn:visible,.tag-passing:visible');
+		if(data['turn_started'] || data['passed']) {
 			// Update the turn clock every second.
 			// There will only ever be one visible turn tag
 			start = data['turn_started'] + time_off;
 			turn_clock = null;
 			update_clock = function() {
-				anchor.html('Turn<br>' + msToString(Date.now() - start));
+				txt = anchor.hasClass('tag-turn') ? 'Turn' : anchor.hasClass('tag-passing') ? 'Passing' : '???';
+				anchor.html(txt + '<br>' + msToString(Date.now() - start));
 			};
 			turn_clock = setInterval(update_clock, 1000);
 			update_clock();
@@ -260,7 +263,7 @@ $(document).ready(function() {
 			anchor.text('Turn');
 		}
 
-		// For asthetic reasons, we only leave enough room for two rows of tags above each card, and turn takes both.
+		// For aesthetic reasons, we only leave enough room for two rows of tags above each card, and turn takes both.
 		// If the current player is also the leader, we hide that tag (it should be obvious they're leading since it's their turn and nobody has played)
 		if(data['leader'] == data['turn']) {
 			$('.tag-lead').hide();

commit c1a32181c37d218cc0482adcdad88913a48c71d1

Fix a problem parsing old logs introduced by 6ead673daaf21c4038df147c02702dbff59d4f57

GameConstructor.py
@@ -115,6 +115,9 @@ class GameConstructor:
 				self.state = 'playing'
 				# Re-pump this event in the playing state
 				return self.pump(event)
+			# In older logs, there was no final bid before going into passing, so we see those events during bidding now. Since it's only needed for showing pass state before round 1 starts, we can ignore it in old logs
+			if event['type'] in ('passing', 'passed'):
+				return
 
 			self.mismatch(event)
 

Shim.py
@@ -51,7 +51,7 @@ class Shim:
 		return line
 
 	@staticmethod
-	def onEvent(gameCon,offset, event):
+	def onEvent(gameCon, offset, event):
 		# Problem where team names weren't saved. The team name in this event is 'dick sledge', but it should've been 'asdf'
 		if gameCon.logFilename == '20160116_035923.log' and offset == 7483:
 			event['team'] = 'asdf'